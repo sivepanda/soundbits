7ef79121729da9741c11f01aa3dee03c7af75a81
sivepanda <sivenpanda@icloud.com>
2023-04-17 03:16:55 -0500

    test

--- Soundbits/app/screens/WelcomeScreen.js
+++ Soundbits/app/screens/WelcomeScreen.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet, Image} from 'react-native';
 
 import { withExpoSnack } from 'nativewind';
 import { styled } from "nativewind";
 
 const StyledText = styled(Text);
+const StyledImage = styled(Image);
+const StyledView = styled(View);
+// const StyledButton = styled(Button);
 
 function WelcomeScreen(props) {
     return (
-        <View style={{flex:1, backgroundColor: "dodgerblue",}} >
-            <StyledText style={{flex:1}} tw='bg-green-600'>Yo, this is cool</StyledText>
-            <Button title="Blank Button"/>
-       </View>
+        <StyledView style={styles.viewContainer} tw="bg-black">
+            <StyledImage style={styles.styTex} source={require('../assets/icon.png')} tw='bg-contain w-[40vw] h-[40vw]'/>
+            <StyledText style={styles.styTex} tw='font-sans text-white text-3xl'>Soundbits</StyledText>
+            {/* <StyledButton style={styles.styBut} title="Log In" nw= { sty }/> */}
+       </StyledView>
     );
 }
+
+// const sty = nw`bg-blue-500 text-white`;
 
 const styles = StyleSheet.create({
     viewContainer: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'space-between'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     styTex: {
 
