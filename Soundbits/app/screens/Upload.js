4f9642b1bba55575b3b6174a1add4cf357c77247
Ahmad Choudhary <chahmad2005@gmail.com>
2023-04-19 03:13:53 -0500

    oof

---
+++ Soundbits/app/screens/Upload.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import { withExpoSnack } from 'nativewind';
+import { styled } from "nativewind";
+import { useFonts } from 'expo-font';
+
+const Upload = () => {
+
+    const navigation = useNavigation(); 
+    
+    return(
+
+        <View style = {styles.overall}>
+            <form>
+                <label>Enter your name:
+                    <input type="text" />
+                </label>
+            </form>
+        </View>
+        
+
+
+    )
+
+    
+}
+
+const styles = StyleSheet.create({
+
+    overall: {
+        display: 'grid',
+
+    },
+
+});
+
+export default withExpoSnack(Accounts);
