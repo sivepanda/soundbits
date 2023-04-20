10659181cda4d6bef340393d348a7e07809016e5
Hyperion3224 <shrimpman911@gmail.com>
2023-04-19 23:29:35 -0500

    the final push

--- Soundbits/app/components/Nav.js
+++ Soundbits/app/components/Nav.js
@@ -2,25 +2,57 @@ import React, {Component} from 'react';
 import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
 import { withExpoSnack } from 'nativewind';
 import { styled } from "nativewind";
+import { Ionicons } from '@expo/vector-icons';
 
-class Nav extends Component {
-  return(
-    
-  );
-}
+const NavBar = ({ navigation, activeTab }) => {
+  const navigateTo = (screenName) => {
+    navigation.navigate(screenName);
+  };
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => navigateTo('Home')} style={styles.tab}>
+          <Ionicons name="home-outline" size={28} color={activeTab === 'Home' ? '#000' : '#ccc'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigateTo('Search')} style={styles.tab}>
+          <Ionicons name="search-outline" size={28} color={activeTab === 'Search' ? '#000' : '#ccc'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigateTo('Post')} style={styles.tab}>
+          <Ionicons name="add-circle-outline" size={28} color={activeTab === 'Post' ? '#000' : '#ccc'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigateTo('Friends')} style={styles.tab}>
+          <Ionicons name="people-outline" size={28} color={activeTab === 'Friends' ? '#000' : '#ccc'} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigateTo('Accounts')} style={styles.tab}>
+          <Ionicons name="person-outline" size={28} color={activeTab === 'Accounts' ? '#000' : '#ccc'} />
+        </TouchableOpacity>
+      </View>
+    );
+};
 
 const styles = StyleSheet.create({
+  // container: {
+  //   flex: 0.7,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
+  // button: {
+  //   alignItems: 'center',
+  //   backgroundColor: '#DDDDDD',
+  //   padding: 10,
+  //   marginBottom: 10,
+  //},
   container: {
-    flex: 0.7,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
+    height: 50,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc'
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-    marginBottom: 10,
-  },
+  tab: {
+    flex: 1,
+    alignItems: 'center'
+  }
 });
 
-export default Nav;
+export default withExpoSnack(NavBar);
