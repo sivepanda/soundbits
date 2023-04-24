b49f35791e260c9535b21ae8a8e7472186f016fc
Ahmad Choudhary <chahmad2005@gmail.com>
2023-04-23 21:34:27 -0500

    ?

--- Soundbits/app/screens/Upload.js
+++ Soundbits/app/screens/Upload.js
@@ -1,37 +1,56 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+// import { View, Text, Image, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { withExpoSnack } from 'nativewind';
 import { styled } from "nativewind";
 import { useFonts } from 'expo-font';
 
+import ReactDOM from 'react-dom/client';
+
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+
 const Upload = () => {
 
-    const navigation = useNavigation(); 
     
-    return(
-
-        <View style = {styles.overall}>
-            <form>
-                <label>Enter your name:
-                    <input type="text" />
-                </label>
-            </form>
+    const [currency, setCurrency] = useState('US Dollar');
+    const navigation = useNavigation(); 
+    return (
+      <View >
+        <Text > Demo Form </Text>
+        <View style={styles.overall} >
+          <TextInput 
+            placeholder="Email" />
+          <TextInput
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+          <Picker
+            selectedValue={currency}
+            onValueChange={currentCurrency => setCurrency(currentCurrency)}>
+            <Picker.Item label="USD" value="US Dollars" />
+            <Picker.Item label="EUR" value="Euro" />
+            <Picker.Item label="NGN" value="Naira" />
+          </Picker>
+          <Text>
+            Selected: {currency}
+          </Text>
         </View>
-        
-
-
-    )
+      </View>
+    );
 
     
 }
+
+
 
 const styles = StyleSheet.create({
 
     overall: {
         display: 'grid',
-
+        gridrow: "20vh 20vh 20vh 20vh",
     },
 
 });
