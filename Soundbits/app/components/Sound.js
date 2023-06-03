d7a459f368cbe1ef93dc7f54e0b060af8e4cd54b
sivepanda <sivenpanda@icloud.com>
2023-06-03 17:43:45 -0500

    made sound work

--- Soundbits/app/components/Sound.js
+++ Soundbits/app/components/Sound.js
@@ -3,6 +3,8 @@ import {StyleSheet, TouchableOpacity, Text, View, Button, Image} from 'react-nat
 import { withExpoSnack } from 'nativewind';
 import { styled } from "nativewind";
 import { Ionicons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
+
 
 const StyledText = styled(Text);
 const StyledView = styled(View);
@@ -16,11 +18,39 @@ const Sound = (props) => {
   const handleDownload = () => {
     setIsDownloaded(true);
   }
+  
+  const [sound, setSound] = React.useState();
+
+    async function playSound() {
+      sound ? await sound.unloadAsync() : null;
+      console.log('Loading Sound');
+      const { sound } = await Audio.Sound.createAsync( require('../assets/meta.mp3')
+      );
+      Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        playsInSilentModeIOS: true,
+      });
+      setSound(sound);
+  
+      console.log('Playing Sound');
+      await sound.playAsync();
+    }
+  
+    React.useEffect(() => {
+      return sound
+        ? () => {
+            console.log('Unloading Sound');
+            sound.unloadAsync();
+          }
+        : undefined;
+    }, [sound]);  
 
     return(
         <StyledView style={styles.box} tw="rounded-xl h-[9vh] w-[90vw] mb-6 bg-gray-400 shadow-sm">
             <View style={styles.colplay}>
-                <StyledImage style={styles.plbt} source={require('../assets/play-button.png')} tw='bg-contain ml-4 w-[5vh] h-[5vh]'/>
+                <TouchableOpacity style={styles.plbt} onPress={playSound}>
+                    <StyledImage style={styles.plbt} source={require('../assets/play-button.png')} tw='bg-contain ml-4 w-[5vh] h-[5vh]'/>
+                </TouchableOpacity>
                 <StyledText style={styles.mono} tw="mt-1 ml-4">{props.tm}</StyledText>
             </View>
             <View style={styles.colinfo}>
--- Soundbits/app/screens/Home.js
+++ Soundbits/app/screens/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Button, View, Text, StyleSheet, Image} from 'react-native';
+import {Button, View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 
 import { withExpoSnack } from 'nativewind';
+import { Ionicons } from '@expo/vector-icons';
 import { styled } from "nativewind";
 import { useFonts } from 'expo-font';
 
@@ -9,9 +10,7 @@ import { uniqueNamesGenerator, adjectives, names, colors, animals } from 'unique
 
 import { useNavigation } from '@react-navigation/native';
 
-//import Sound from 'react-native-sound';
 
-//import TrackPlayer from 'react-native-track-player';
 
 
 import Sounds from '../components/Sound'
@@ -24,81 +23,6 @@ const StyledView = styled(View);
 const StyledButton = styled(Button);
 const StyledTitle = styled(Title);
 
-/* -------------------------------------Sound------------------------------- 
-https://blog.logrocket.com/how-to-play-sounds-in-react-native-using-react-native-sound/
-docs used
-
----PROBLEM: NEED TO LINK IOS AND REACT NATIVE SOUND
----PREVIOSLY YOUèd AUTOLINK WITH react-native link react-native-sound IN CMD
----ITS WAY STUPIDER NOW
-
-Sound.setCategory('Playback');
-
-var metaa = new Sound('../assets.meta.mp3', Sound.MAIN_BUNDLE, (error) => {
-    if (error) {
-      console.log('failed to load the sound', error);
-      return;
-    }
-    // when loaded successfully
-    console.log('duration in seconds: ' + metaa.getDuration() + 'number of channels: ' + metaa.getNumberOfChannels());
-  });
-
-  const playPause = () => {
-    metaa.play(success => {
-      if (success) {
-        console.log('successfully finished playing');
-      } else {
-        console.log('playback failed due to audio decoding errors');
-      }
-    });
-  };
-
-   -------------------------------------------------------------------------- */
-
-/* ---------------------------------TrackPlayer-------------------------------------- 
-
-
-Use this instead of react native sound
-add this: TrackPlayer.registerPlaybackService(() => require('./service')); to index.js
-or this: 
-
-import TrackPlayer from 'react-native-track-player';
-AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => require('./service'));
-
-to index.js
-
-https://github.com/doublesymmetry/react-native-track-player/issues/932 
-to get this to work
-
-https://react-native-track-player.js.org/docs/basics/getting-started/
-official TrackPlayer docs
-
-TrackPlayer.setupPlayer()
-
-const track1 =  {
-    url: require('../assets/meta.mp3'), // Load media from the app bundle
-    title: 'Metamorposis',
-    artist: 'Jaxon',
-    artwork: require('../assets/icon.png'), // Load artwork from the app bundle
-    duration: 90
-};
-
- TrackPlayer.add(track1);
-
-const state = TrackPlayer.getState();
-if (state === State.Playing) {
-    console.log('The player is playing');
-};
-
-let trackIndex = TrackPlayer.getCurrentTrack();
-let trackObject = TrackPlayer.getTrack(trackIndex);
-console.log(`Title: ${trackObject.title}`);
-
-const position = TrackPlayer.getPosition();
-const duration = TrackPlayer.getDuration();
-console.log(`${duration - position} seconds left.`);
- -------------------------------------------------------------------------- */
 
 
 const config = {
@@ -119,8 +43,10 @@ function randNum() {
     return min + ":" + sec;
 }
 
+
 function Home() {
     const navigation = useNavigation();
+
     return (
         <StyledView style={styles.viewContainer}>
             <StyledView tw="mt-[-70vh]" style={styles.head}>
@@ -129,12 +55,6 @@ function Home() {
                 <View style={{alignItems: 'left'}}>
                         <StyledText tw="text-4xl color-white pt-[2vh] pb-[2vh] font-bold">Explore Sounds:</StyledText>
                 </View>
-               {/*
-               IS A PLAY SOUND BUTTON (meant to be one)
-                <TouchableOpacity style={styles.playBtn} onPress={playPause}>
-                    <Ionicons name={'ios-play-outline'} size={36} color={'#fff'} />
-                </TouchableOpacity>
-               */}
             <Sounds tm={ "1:31" } nm={ "Metamorphisis" } auth={"Jaxon Durken"}/>
             <Sounds tm={ randNum() } nm={ uniqueNamesGenerator(config_D) } auth={uniqueNamesGenerator(config) + " " + uniqueNamesGenerator(config)}/>
             <Sounds tm={ randNum() } nm={ uniqueNamesGenerator(config_D) } auth={uniqueNamesGenerator(config) + " " + uniqueNamesGenerator(config)}/>
