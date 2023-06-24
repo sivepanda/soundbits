427e2cc263921b63f407764f983c73ce14f82d36
Hyperion3224 <shrimpman911@gmail.com>
2023-06-24 14:07:20 -0500

    fixed get method

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -9,6 +9,25 @@ router.get("/", async (req, res) => {
     res.json(get);
 })
 
+app.get('/:id/:attribute', (req, res) => {
+    const id = req.params.id;
+    const attribute = req.params.attribute;
+  
+    User.findByPk(id, {
+      attributes: [attribute] // Pass the attribute dynamically
+    })
+      .then((result) => {
+        if (result) {
+          res.json(result);
+        } else {
+          res.status(404).json({ error: 'Record not found' });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'Internal server error' });
+      });
+  });
+
 router.post("/", async (req, res) => {
     const post = req.body;
     await User.create(post);
