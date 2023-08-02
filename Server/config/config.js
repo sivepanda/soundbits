e7d59606d92b82b92799c20e14280e7446a104a0
sivepanda <sivenpanda@icloud.com>
2023-08-02 03:31:26 -0500

    e

--- Server/config/config.js
+++ Server/config/config.js
@@ -4,21 +4,21 @@ module.exports = {
     "username": process.env.AWS_USR,
     "password": process.env.PASS,
     "database": process.env.AWS_DB,
-    "host": process.env.HOSTNM,
+    "host": 'soundbits.cvidf6oikyfm.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.AWS_USR,
     "password": process.env.PASS,
     "database": process.env.AWS_DB,
-    "host": process.env.HOSTNM,
+    "host": 'soundbits.cvidf6oikyfm.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
   },
   "production": {
     "username": process.env.AWS_USR,
     "password": process.env.PASS,
     "database": process.env.AWS_DB,
-    "host": process.env.HOSTNM,
+    "host": 'soundbits.cvidf6oikyfm.us-east-1.rds.amazonaws.com',
     "dialect": "mysql"
   }
 };
